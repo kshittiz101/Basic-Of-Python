e72f47f167d1b219d6629d9cf791487672a05af6
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-12-03 06:18:49 +0545

    feat: set different methods

--- datatypes/01-sets/set3.py
+++ datatypes/01-sets/set3.py
@@ -1,1 +1,24 @@
-# method operations in sets
+# Mathematical Set Operations
+
+# 1. Union ( | )
+# Combines all items from both sets (removes duplicates).
+a = {1, 2, 3, 4, 5}
+b = {2, 3, 4, 5, 6, 7}
+union_of_sets = a | b
+print(union_of_sets)
+
+# 2. Intersection (`&`)
+# Keeps only items present in both sets.
+common_value = a & b
+print(f"common value for sets : {common_value}")
+
+# 3.Difference ( - )
+# Keeps items in the first set that are not in the second set.
+only_a = a - b
+print(f"the only a element: {only_a}")
+
+
+# 4. Symmetric Difference (`^`)
+# Keeps items that are in either set, but not in both.
+symmentric_diff = a ^ b
+print(symmentric_diff)
