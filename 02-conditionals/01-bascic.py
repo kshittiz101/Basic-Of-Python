2c91f53289139193e0dbb2022ded4bc48a0a6d1d
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-12-16 21:30:13 +0545

    basic of conditionals

--- 02-conditionals/01-bascic.py
+++ 02-conditionals/01-bascic.py
@@ -1,72 +1,24 @@
-#condtional statement ---making decision based variable hold or result
-#if statement
-#syntax:
-# if condtion:True
-#     #block of code 
-# else:
-     #block of code
-   
+# conditional statements in python
+# 1. if statement
+age = 12
+if age >= 18:
+    print("You are eligible to vote.")
+
+# 2. if-else statement
+score = 90
+if score >= 50:
+    print("You have passed the exam.")
+else:
+    print("You have failed the exam.")
 
 
-# a=20
-# # print(a>15)
-# if a>50:
-#     print("hello i want to coffee shop")
-# else:
-#     print(" i donot wanna  go coffee shop")
-
-
-# print('some imp code ')
-
-# if else in one line statement
-# a=10
-# if a>5:print("a is greater than 5")
-
-#short hand if else statement (ternary operator)
-
-# a=10
-# if a>20:
-#     print("a is greater than 5")
-# else:
-#     print("a is small number")
-
-# a=10
-# print("a is greater than 5") if a>2 else print("a is small number")
-
-#nested
-
-# age=int(input("enter your age : "))
-
-# if age<18:
-#     print("you have to pay Rs 20")
-
-# elif age>=18 and age<40:
-#     if age==30:
-#         print("you should not pay ")
-#     else:
-#         print("you have to pay Rs 100")
-# else:
-#     print("you have to pay Rs 50")
-
-
-# num=int(input("enter you any number : "))
-# if num%2==0:
-#     print("this is even")
-# else:
-#     print("this is odd number ")
-
-
-# a="carac"
-
-# a="aeiou"
-# character=input("enter your character : ")
-# character=character.lower()
-
-# if character in a:
-#     print("yes")
-
-# else:
-#     print("no")
-
-
-
+# 3. if-elif-else statement
+marks = 65
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")
--- 02-conditionals/game.py
+++ 02-conditionals/game.py
@@ -1,26 +1,25 @@
-#game 
-a="welcome to our game"
-print(a.center(70,"-"))
+# game
+import random
+a = "welcome to our game"
+print(a.center(70, "-"))
 print("S for Sissor P for Papper R for Rock")
 print("remember!!! S>P , P>R R>S")
 
-user=input("enter your choice : ")
-user=user.upper()
+user = input("enter your choice : ")
+user = user.upper()
 
-import random
-computer=random.choice(['S','P','R'])
+computer = random.choice(['S', 'P', 'R'])
 
 print(f" You choose a : {user}")
 print(f"computer choose :{computer}")
 
-if (user=='S' and computer=='P') or (user=='P' and computer=='R') or\
-    (user=='R' and computer=='S'):
+if (user == 'S' and computer == 'P') or (user == 'P' and computer == 'R') or\
+        (user == 'R' and computer == 'S'):
     print("You won!!!")
 
-elif user==computer:
+elif user == computer:
     print("draw!!!")
-elif user not in ['S','P','R']:
+elif user not in ['S', 'P', 'R']:
     print("please enter valid keyword!!!!")
 else:
     print("computer won!!!")
-
