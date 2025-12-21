818ed6906b67024f7b191864d819759eb9d6d75c
kshittiz101 <chaudharykshittiz950@gmail.com>
2025-12-21 22:33:39 +0545

    feat: different method in tuple

--- 01-datatypes/03-tuples/01-tuple.py
+++ 01-datatypes/03-tuples/01-tuple.py
@@ -29,7 +29,28 @@ print(f"First element: {third_tuple[0]}")
 print(f"last element: {third_tuple[-1]}")
 
 # update: we can not update the value of the tuple becausse it is immutable
-third_tuple[0] = 200
+# third_tuple[0] = 200
 
 # TypeError: 'tuple' object does not support item assignment
 # print(third_tuple)
+
+# in python we cannot able to delete individaul element of the tuple, because it is immutable in nature
+roles = ("manager", "developer", "graphic designers ")
+#  we have to delete the whole tuple
+del roles
+# print(roles)
+
+
+# If you want to remove elements
+# Convert the tuple to a list, modify it, then convert back:
+fruits = ("apples", "orange", "mango", "banana")
+print(f"Elements of fruits: {fruits}, and types is {type(fruits)}")
+
+# converting tuples into list
+list_of_fruits = list(fruits)
+print(type(list_of_fruits))
+
+# here in the list of fruits remove() function will remove first occurance of the friut from list
+# remoce work on list not in tuple
+list_of_fruits.remove("banana")
+print(list_of_fruits)
---
+++ 01-datatypes/03-tuples/02-tuple.py
@@ -0,0 +1,1 @@
+# important method in tuple
